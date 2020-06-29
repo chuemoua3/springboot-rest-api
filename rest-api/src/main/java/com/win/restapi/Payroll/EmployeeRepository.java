f816654a9247d2c30bcd1611d95af105adeb316e
chuemoua3 <chuemoua12@outlook.com>
2020-06-29 00:41:23 -0400

    finished

---
+++ rest-api/src/main/java/com/win/restapi/Payroll/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.win.restapi.Payroll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
---
+++ rest-api/src/main/java/com/win/restapi/Payroll/Status.java
@@ -0,0 +1,7 @@
+package com.win.restapi.Payroll;
+
+public enum Status {
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED    
+}
